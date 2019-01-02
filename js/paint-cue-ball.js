9214fe14538fd12709aec44c59837dddecd2ad1b
Harrison McCullough <mccullough.harrison@gmail.com>
2019-01-02 10:11:34 -0700

    Remove unused files

--- js/paint-cue-ball.js
+++
@@ -1,8 +0,0 @@
-const paintCircle = require('./paint-circle');
-const CUE_BALL_COLOR = 'white';
-
-function paintCueBall(ctx, cueBall) {
-  paintCircle(ctx, cueBall.circle, CUE_BALL_COLOR);
-}
-
-module.exports = paintCueBall;
