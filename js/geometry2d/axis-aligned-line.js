8c52cdf9a8a1e71c647901f0898459f497879ac0
Harrison McCullough <mccullough.harrison@gmail.com>
2019-01-11 09:22:10 -0700

    Make instance variables private in AxisAlignedLine

--- js/geometry2d/axis-aligned-line.js
+++ js/geometry2d/axis-aligned-line.js
@@ -3,9 +3,21 @@
 class AxisAlignedLine {
 
   constructor(corner, direction, length) {
-    this.corner = corner;
-    this.direction = direction;
-    this.length = length;
+    this._corner = corner;
+    this._direction = direction;
+    this._length = length;
+  }
+
+  get corner() {
+    return this._corner;
+  }
+
+  get direction() {
+    return this._direction;
+  }
+
+  get length() {
+    return this._length;
   }
 
 }
