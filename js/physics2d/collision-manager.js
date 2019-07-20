d4805285365b5c2fd26976e5d5b68c3e110d883f
Harrison McCullough <mccullough.harrison@gmail.com>
2019-07-19 23:44:21 -0600

    Changes I forgot about

--- js/physics2d/collision-manager.js
+++ js/physics2d/collision-manager.js
@@ -13,33 +13,40 @@ class CollisionManager {
   doAllCollisions() {
     let allCollidedBalls = [];
     let recentlyCollidedBalls = new Set();
-    this.updateCollidedBalls(recentlyCollidedBalls, this.balls);
-    this.updateAllCollidedBalls(recentlyCollidedBalls, allCollidedBalls);
+    this.collideBalls(recentlyCollidedBalls, this.balls);
+    this.updateCollidedBalls(recentlyCollidedBalls, allCollidedBalls);
+    let index = 0;
     while (!SetUtil.empty(recentlyCollidedBalls)) {
       let prevCollidedBalls = recentlyCollidedBalls;
       recentlyCollidedBalls = new Set();
-      this.updateCollidedBalls(recentlyCollidedBalls, prevCollidedBalls);
-      this.updateAllCollidedBalls(recentlyCollidedBalls, allCollidedBalls);
+      this.collideBalls(recentlyCollidedBalls, prevCollidedBalls);
+      this.updateCollidedBalls(recentlyCollidedBalls, allCollidedBalls);
+      index += 1;
+      if (index > 20) {
+        console.log("Too many times...");
+        break;
+      }
+      console.log("index: ", index);
     }
+    console.log("done.");
     // return ArrayUtil.count(allCollidedBalls);
     let count = ArrayUtil.count(allCollidedBalls);
     return count;
   }
 
-  updateCollidedBalls(recentlyCollidedBalls, balls) {
+  collideBalls(recentlyCollidedBalls, balls) {
     let ballsHitWalls = this.ballsHitWalls(this.balls);
     SetUtil.extend(recentlyCollidedBalls, ballsHitWalls);
     let ballsHitBalls = this.ballsHitBalls(this.balls);
     SetUtil.extend(recentlyCollidedBalls, ballsHitBalls);
   }
 
-  updateAllCollidedBalls(recentlyCollidedBalls, allCollidedBalls) {
+  updateCollidedBalls(recentlyCollidedBalls, allCollidedBalls) {
     allCollidedBalls.push(...recentlyCollidedBalls);
   }
 
   ballsHitWalls(balls) {
-    return balls.filter(ball =>
-                                this.ballHitWalls(ball));
+    return balls.filter(ball => this.ballHitWalls(ball));
   }
 
   ballHitWalls(ball) {
@@ -54,10 +61,16 @@ class CollisionManager {
 
   ballsHitBalls(balls) {
     let recentlyCollidedBalls = new Set();
+    // balls.forEach(
+    //   (ball, index, array) =>
+    //     SetUtil.extend(recentlyCollidedBalls,
+    //                    this.ballHitBalls(ball, array.slice(index + 1)))
+    // );
     balls.forEach(
       (ball, index, array) =>
         SetUtil.extend(recentlyCollidedBalls,
-                       this.ballHitBalls(ball, array.slice(index + 1)))
+                       // this.ballHitBalls(ball, array.slice(index + 1)))
+                       this.ballHitBalls(ball, array))
     );
     return recentlyCollidedBalls;
   }
